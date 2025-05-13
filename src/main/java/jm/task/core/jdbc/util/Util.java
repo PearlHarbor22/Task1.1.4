fbd67b036612da803839ef9486d5092b1b017b20
Nikita Kalashnikov <kalashi47_n12@mail.ru>
2025-05-13 23:18:09 +0700

    Правки к задаче 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/db_bipki";
@@ -20,5 +26,34 @@ public class Util {
             System.err.println("Ошибка подключения к базе данных" + e.getMessage());
         }
     return null;
+    }
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, DB_URL);
+                settings.put(Environment.USER, DB_USER);
+                settings.put(Environment.PASS, DB_PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "none");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                sessionFactory = configuration.buildSessionFactory();
+                System.out.println("Hibernate SessionFactory создан нами успешно");
+            } catch (Exception e) {
+                System.out.println("Ошибка при создании Hibernate SessionFactory" + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
